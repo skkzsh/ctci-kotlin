3df8e51c4ce263258d7346f8175758494c208ecf
skkzsh <skkzsh12@gmail.com>
2023-04-18 22:52:47 +0900

    (WIP) 2.1

--- src/main/kotlin/dev/skkzsh/ctci/q02/LinkedListNode02.kt
+++ src/main/kotlin/dev/skkzsh/ctci/q02/LinkedListNode02.kt
@@ -3,7 +3,24 @@ package dev.skkzsh.ctci.q02
 import dev.skkzsh.dsa.linkedlist.TailLinkedListNode
 
 class LinkedListNode02(value: Int) : TailLinkedListNode(value) {
+
     fun deleteDups() {
-        throw UnsupportedOperationException("未実装")
+        val set: MutableSet<Int> = mutableSetOf()
+
+        var n: TailLinkedListNode = this
+        set.add(n.value)
+//        println(set)
+
+        // FIXME: 境界値 (last)
+        while (n.next != null) {
+            println(set)
+            if (set.contains(n.next!!.value)) {
+                n.next = n.next!!.next
+                length--
+            } else {
+                set.add(n.next!!.value)
+            }
+            n = n.next!!
+        }
     }
 }
--- src/test/kotlin/dev/skkzsh/ctci/q02/LinkedListNode02Test.kt
+++ src/test/kotlin/dev/skkzsh/ctci/q02/LinkedListNode02Test.kt
@@ -1,23 +1,42 @@
 package dev.skkzsh.ctci.q02
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 import kotlin.test.assertEquals
 
 class LinkedListNode02Test {
     private val n = LinkedListNode02(100)
 
-    @BeforeEach
-    fun setUp() {
+    @Test
+    fun deleteDups() {
+        n.append(101)
+        n.append(100)
+        n.append(102)
+
+        // n.print() // debug
+        n.deleteDups()
+
+        n.print() // debug
+        assertEquals(3, n.length())
+        assertEquals(100, n.at(0))
+        assertEquals(101, n.at(1))
+        assertEquals(102, n.at(2))
+    }
+
+    @Ignore
+    @Test
+    fun deleteDups_last() {
         n.append(101)
         n.append(102)
         n.append(100)
-    }
 
-    @Test
-    fun deleteDups() {
+        // n.print() // debug
         n.deleteDups()
-        assertEquals(101, n.at(0))
-        assertEquals(102, n.at(1))
+
+        n.print() // debug
+        assertEquals(3, n.length())
+        assertEquals(100, n.at(0))
+        assertEquals(101, n.at(1))
+        assertEquals(102, n.at(2))
     }
 }
