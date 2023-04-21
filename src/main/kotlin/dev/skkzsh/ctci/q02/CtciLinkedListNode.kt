48dcf92f8cd2a34215efe018f6b481124597bcf6
skkzsh <skkzsh12@gmail.com>
2023-04-21 21:03:05 +0900

    Solve 2.2

---
+++ src/main/kotlin/dev/skkzsh/ctci/q02/CtciLinkedListNode.kt
@@ -0,0 +1,9 @@
+package dev.skkzsh.ctci.q02
+
+import dev.skkzsh.dsa.linkedlist.TailLinkedListNode
+
+class CtciLinkedListNode(value: Int) : TailLinkedListNode(value) {
+    fun atBack(num: Int): Int {
+        return at(length - num)
+    }
+}
---
+++ src/test/kotlin/dev/skkzsh/ctci/q02/CtciLinkedListNodeTest.kt
@@ -0,0 +1,29 @@
+package dev.skkzsh.ctci.q02
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.assertEquals
+
+class CtciLinkedListNodeTest {
+    private val n = CtciLinkedListNode(100)
+
+    @BeforeEach
+    fun setUp() {
+        n.append(101)
+        n.append(102)
+        n.append(103)
+        n.append(104)
+
+        // n.print() // debug
+    }
+
+    @Test
+    fun atBack() {
+        assertEquals(104, n.atBack(1))
+        assertEquals(103, n.atBack(2))
+        assertEquals(102, n.atBack(3))
+        assertEquals(101, n.atBack(4))
+        assertEquals(100, n.atBack(5))
+    }
+}
