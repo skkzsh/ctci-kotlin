eeed1042740a66f5965dd72f487964ebd1af618b
skkzsh <skkzsh12@gmail.com>
2023-08-20 12:00:05 +0900

    Test 2.3

--- src/test/kotlin/dev/skkzsh/ctci/q02/Ctci03LinkedListNodeTest.kt
+++ src/test/kotlin/dev/skkzsh/ctci/q02/Ctci03LinkedListNodeTest.kt
@@ -2,6 +2,7 @@ package dev.skkzsh.ctci.q02
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class Ctci03LinkedListNodeTest {
@@ -12,21 +13,44 @@ class Ctci03LinkedListNodeTest {
         n.append(101)
         n.append(102)
         n.append(103)
-        n.append(104)
 
         // n.print() // debug
     }
 
     @Test
     fun deleteMid() {
+        n.deleteMid(102)
+        // n.print() // debug
+
+        assertEquals(3, n.length())
+        assertEquals(100, n.at(0))
+        assertEquals(101, n.at(1))
+        assertEquals(103, n.at(2))
+    }
+
+    @Test
+    fun deleteMid_Last() {
         n.deleteMid(103)
+        // n.print() // debug
+
+        assertEquals(3, n.length())
+        assertEquals(100, n.at(0))
+        assertEquals(101, n.at(1))
+        assertEquals(102, n.at(2))
+    }
+
+    @Test
+    fun deleteMid_NotExist() {
+        assertThrows<NullPointerException> {
+            n.deleteMid(99)
+        }
+
         // n.print() // debug
 
         assertEquals(4, n.length())
         assertEquals(100, n.at(0))
         assertEquals(101, n.at(1))
         assertEquals(102, n.at(2))
-        assertEquals(104, n.at(3))
+        assertEquals(103, n.at(3))
     }
-
 }
